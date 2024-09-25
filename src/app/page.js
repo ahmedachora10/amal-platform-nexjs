4071bf5b0b57bb30e95d96469200e77ca8d4c10b
mostafa mohammed <000madz000@gmail.com>
2024-09-25 04:27:44 +0300

    fix sections design error

--- src/app/page.js
+++ src/app/page.js
@@ -34,30 +34,32 @@ export default function Home() {
           </div>
         </div> */}
 
-        <section className="container mx-auto justify-between flex flex-col lg:flex-row relative">
+        <section className="container mx-auto justify-between flex flex-col xl:flex-row relative">
           <Image src={Bg1} className="absolute top-0 left-0 -translate-x-[25%] -translate-y-[25%]" draggable={false} />
 
-          <div className="lg:w-[40.83%] flex flex-col gap-7">
-            <h1 className="lg:text-[80px] text-[40px] font-bold">Learn Arabic with us from the best.</h1>
-            <p className="lg:text-[24px] text-[16px] text-[#707070]">We help your student study better with their own pace. Join us in our mission to create a better future for the kids.</p>
-            <Button className="lg:w-[12.5vw] w-fit">Get Started</Button>
+          <div className="xl:w-[40.83%] flex flex-col items-center gap-7">
+            <h1 className="xl:text-[80px] text-[40px] font-bold">Learn Arabic with us from the best.</h1>
+            <p className="xl:text-[24px] text-[16px] text-[#707070]">We help your student study better with their own pace. Join us in our mission to create a better future for the kids.</p>
+            <div className="flex items-start w-full">
+              <Button className="xl:w-[12.5vw] w-fit">Get Started</Button>
+            </div>
           </div>
 
-          <div className="lg:w-[41.72%] z-10 lg:translate-y-[20%]">
+          <div className="xl:w-[41.72%] z-10 xl:translate-y-[20%] flex justify-center">
             <Image src={Bg2} draggable={false} />
           </div>
         </section>
 
 
-        <section className="bg-[#EBF3FF] px-7 pb-7 pt-80 -translate-y-[50%] lg:p-7">
-          <div className="container mx-auto lg:flex grid grid-cols-2 gap-7">
+        <section className="bg-[#EBF3FF] px-7 pb-7 pt-80 -translate-y-[50%] xl:-translate-y-[0%] xl:p-7">
+          <div className="container mx-auto xl:flex grid grid-cols-2 gap-7">
 
-            <div className="lg:max-w-[12.5%] flex flex-col gap-4">
+            <div className="xl:max-w-[12.5%] flex flex-col gap-4">
               <Image src={ScholarImg} />
               <TextSection title="Arabic Grammer" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
             </div>
 
-            <div className="lg:max-w-[12.5%] flex flex-col gap-4">
+            <div className="xl:max-w-[12.5%] flex flex-col gap-4">
               <Image src={ScienceImg} />
               <TextSection title="Sharia sciences" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
             </div>
@@ -65,7 +67,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="container mx-auto bg-[#EBF3FF] p-7">
+        <section className="container mx-auto bg-[#EBF3FF] p-7 -translate-y-[50%] xl:-translate-y-[0%]">
           <div className="flex justify-between">
             <h1 className="text-3xl font-bold">Our Top Online Courses</h1>
             <Button>View All</Button>
