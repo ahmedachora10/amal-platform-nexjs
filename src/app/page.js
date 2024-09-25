6c6b33431f61e6a462b633638ad482c94d62a8d9
mostafa mohammed <000madz000@gmail.com>
2024-09-25 04:10:26 +0300

    complete 3 sections in home page

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,16 @@ import icon from './test.jpeg';
 import HeadlineWithLink from "@/components/HeadlineWithLink";
 import Course from "@/components/cards/Course";
 import BestSellingCourses from "@/components/sections/BestSellingCourses";
+import Bg1 from "./images/home/bg-1.png";
+import Bg2 from "./images/home/bg-2.png";
+import ScienceImg from "./images/home/Science.png";
+import ScholarImg from "./images/home/Scholar.png";
+import TextSection from "@/components/sections/text_section";
 
 export default function Home() {
   return (
     <div>
-      <div className='container mx-auto'>
+      <div className='container mx-auto flex flex-col gap-14'>
         {/* <Headline title="Platform Features" subTitle="Features" /> */}
         {/* <HeadlineWithLink title="Platform Features" linkName="Features" link="/" /> */}
 
@@ -29,9 +34,44 @@ export default function Home() {
           </div>
         </div> */}
 
-        <div className="container mx-auto">
+        <section className="container mx-auto justify-between flex flex-col lg:flex-row relative">
+          <Image src={Bg1} className="absolute top-0 left-0 -translate-x-[25%] -translate-y-[25%]" draggable={false} />
+
+          <div className="lg:w-[40.83%] flex flex-col gap-7">
+            <h1 className="lg:text-[80px] text-[40px] font-bold">Learn Arabic with us from the best.</h1>
+            <p className="lg:text-[24px] text-[16px] text-[#707070]">We help your student study better with their own pace. Join us in our mission to create a better future for the kids.</p>
+            <Button className="lg:w-[12.5vw] w-fit">Get Started</Button>
+          </div>
+
+          <div className="lg:w-[41.72%] z-10 lg:translate-y-[20%]">
+            <Image src={Bg2} draggable={false} />
+          </div>
+        </section>
+
+
+        <section className="bg-[#EBF3FF] px-7 pb-7 pt-80 -translate-y-[50%] lg:p-7">
+          <div className="container mx-auto lg:flex grid grid-cols-2 gap-7">
+
+            <div className="lg:max-w-[12.5%] flex flex-col gap-4">
+              <Image src={ScholarImg} />
+              <TextSection title="Arabic Grammer" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+            </div>
+
+            <div className="lg:max-w-[12.5%] flex flex-col gap-4">
+              <Image src={ScienceImg} />
+              <TextSection title="Sharia sciences" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+            </div>
+
+          </div>
+        </section>
+
+        <section className="container mx-auto bg-[#EBF3FF] p-7">
+          <div className="flex justify-between">
+            <h1 className="text-3xl font-bold">Our Top Online Courses</h1>
+            <Button>View All</Button>
+          </div>
           <BestSellingCourses />
-        </div>
+        </section>
 
         {/* <div className="flex">
           <div className="col-3 mx-3">
