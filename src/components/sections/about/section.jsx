0212cc24cd2474278516963aa2268c3440a59b77
mostafa mohammed <000madz000@gmail.com>
2024-09-28 19:54:23 +0300

    fix data is null error

--- src/components/sections/about/section.jsx
+++ src/components/sections/about/section.jsx
@@ -5,27 +5,27 @@ import TextWithYelloTrue from "@/components/yellow_true_txt";
  */
 export default function AboutSection({ data, reverse = false }) {
     return (
-        <section className={`flex flex-col gap-7 ${reverse ? "sm:flex-row-reverse" : "sm:flex-row"} justify-between items-center w-screen container mx-7 sm:mx-auto`}>
+        data ? <section className={`flex flex-col gap-7 ${reverse ? "sm:flex-row-reverse" : "sm:flex-row"} justify-between items-center w-screen container mx-7 sm:mx-auto`}>
             <div className="flex flex-col gap-7">
                 <div className="sm:w-[29.48vw]">
-                    <h1 className="text-2xl">{data.title}</h1>
+                    <h1 className="text-2xl">{data?.title}</h1>
 
                     <p className="text-md font-sans">
-                        {data.description}
+                        {data?.description}
                     </p>
 
                 </div>
 
                 <div className={"[&>p]:flex [&>p]:gap-3 flex flex-col gap-7"}>
-                    {data.properties.map((val, i) => (
+                    {data?.properties.map((val, i) => (
                         <TextWithYelloTrue text={val} key={i} />
                     ))}
                 </div>
             </div>
 
 
-            <img src={data.image} className="sm:w-[29.48%]" />
+            <img src={data?.image} className="sm:w-[29.48%]" />
 
-        </section>
+        </section> : null
     )
 }
