5ed8516b22b848e46cab007d44279427f4625ffb
mostafa mohammed <000madz000@gmail.com>
2024-09-28 21:05:57 +0300

    allow cross origin

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,33 @@ const nextConfig = {
     experimental: {
         urlImports: ['http://127.0.0.1:8000', 'http://localhost:8000', 'https://i.ibb.co'], // Add this if using a non-default port
     },
+    headers: async () => {
+        /**
+         * @type {import('next/dist/lib/load-custom-routes').Header[]}
+         */
+        const data = [
+            {
+                source: "/sanctum/csrf-cookie",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            },
+            {
+                source: "/api/*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+
+        return data;
+    }
 };
 
 
