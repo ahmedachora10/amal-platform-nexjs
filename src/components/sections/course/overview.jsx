06df6e7313b41af6d3fe8b7ed774cd9d2bf56d7e
mostafa mohammed <000madz000@gmail.com>
2024-09-28 03:24:55 +0300

    don't show the lessons overview section when no lessons

--- src/components/sections/course/overview.jsx
+++ src/components/sections/course/overview.jsx
@@ -17,36 +17,39 @@ export default function CourseOverviewSection({ data, lessons }) {
             </div>
 
 
-            <div className="flex flex-col justify-between w-full">
-                <thead className="grow flex">
-                    <div className="text-sm flex gap-3 items-center justify-start grow bg-[#F9F9F9] p-4">
-                        <ChevronUp />
-                        <p>Getting Started</p>
-                    </div>
 
-                    <div className="flex items-center gap-4">
-                        <div className="flex text-sm gap-2 items-center">
-                            <CirclePlay className="text-xs" />
-                            <p>{data.lessons_count || 0} lectures</p>
+            {(lessons && lessons.length) ? (
+                <div className="flex flex-col justify-between w-full">
+                    <thead className="grow flex">
+                        <div className="text-sm flex gap-3 items-center justify-start grow bg-[#F9F9F9] p-4">
+                            <ChevronUp />
+                            <p>Getting Started</p>
                         </div>
 
-                        <div className="flex text-sm gap-2 items-center">
-                            <Clock9Icon className="text-xs" />
-                            <p>{data.duration} min</p>
-                        </div>
-                    </div>
-                </thead>
-                <hr />
+                        <div className="flex items-center gap-4">
+                            <div className="flex text-sm gap-2 items-center">
+                                <CirclePlay className="text-xs" />
+                                <p>{data.lessons_count || 0} lectures</p>
+                            </div>
 
-                <tbody className="flex flex-col">
-                    {lessons?.map((lesson, i) => (
-                        <React.Fragment key={i}>
-                            <CourseLesson data={lesson} />
-                            <hr />
-                        </React.Fragment>
-                    ))}
-                </tbody>
-            </div>
+                            <div className="flex text-sm gap-2 items-center">
+                                <Clock9Icon className="text-xs" />
+                                <p>{data.duration} min</p>
+                            </div>
+                        </div>
+                    </thead>
+                    <hr />
+
+                    <tbody className="flex flex-col">
+                        {lessons?.map((lesson, i) => (
+                            <React.Fragment key={i}>
+                                <CourseLesson data={lesson} />
+                                <hr />
+                            </React.Fragment>
+                        ))}
+                    </tbody>
+                </div>
+            ) : undefined}
         </section>
     )
 }
